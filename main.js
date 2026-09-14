d6470bb277d43966bff25dd47b9fef640d626823
spienikla-stack <spienikla@gmail.com>
2026-09-14 22:00:48 +0200

    Add files via upload

---
+++ main.js
@@ -0,0 +1,33 @@
+const { app, BrowserWindow } = require('electron')
+const path = require('path')
+
+function createWindow () {
+  const win = new BrowserWindow({
+    width: 1280,
+    height: 720,
+    title: "Leitstellen Simulator",
+    autoHideMenuBar: true,
+    webPreferences: {
+      nodeIntegration: true
+    }
+  })
+
+  win.loadFile('index.html')
+  win.maximize()
+}
+
+app.whenReady().then(() => {
+  createWindow()
+
+  app.on('activate', () => {
+    if (BrowserWindow.getAllWindows().length === 0) {
+      createWindow()
+    }
+  })
+})
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit()
+  }
+})
